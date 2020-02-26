5fa59e3d12dd79f028c2187e3a32caaefadcfc94
Zhuang Xu <not-real@example.com>
2020-02-26 14:58:57 +1100

    Set production server configuration

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,4 +1,5 @@
 /* eslint-disable import/prefer-default-export */
 export const baseURL = (env) => (env === 'production'
-  ? 'https://pipchat-server.herokuapp.com' // process.env.REACT_APP_PIPCHAT_SERVER_URL
-  : 'http://localhost:3001');
+  ? process.env.REACT_APP_PIPCHAT_SERVER_URL
+  : 'http://localhost:3001'
+);
