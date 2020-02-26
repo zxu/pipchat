b4ce94368c7479a00588be543bde3af424e4caba
Zhuang Xu <not-real@example.com>
2020-02-26 13:28:32 +1100

    Updates

---
+++ precache-manifest.d8fde52de3be4d3bc8e968b2b831db42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8e19e1bee540cd32b83ebcb5523b80b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ece7422628bd8668bef",
+    "url": "/static/css/2.c4383bce.chunk.css"
+  },
+  {
+    "revision": "70ad05230316503d499c",
+    "url": "/static/css/main.e8f11205.chunk.css"
+  },
+  {
+    "revision": "3ece7422628bd8668bef",
+    "url": "/static/js/2.5d38b069.chunk.js"
+  },
+  {
+    "revision": "79764afef5f0f09c9f8346b4f81f1a6e",
+    "url": "/static/js/2.5d38b069.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70ad05230316503d499c",
+    "url": "/static/js/main.59b0d2da.chunk.js"
+  },
+  {
+    "revision": "5bb0349ac9eb5aeb8233",
+    "url": "/static/js/runtime-main.d63c064e.js"
+  }
+]);
