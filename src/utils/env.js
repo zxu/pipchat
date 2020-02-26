5162700c633ef54d7513f2c0fab3b1c9a760975b
Zhuang Xu <not-real@example.com>
2020-02-26 13:29:19 +1100

    Prepare to deploy to github pages

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,4 +1,4 @@
 /* eslint-disable import/prefer-default-export */
 export const baseURL = (env) => (env === 'production'
-  ? process.env.REACT_APP_PIPCHAT_SERVER_URL
+  ? 'https://pipchat-server.herokuapp.com' // process.env.REACT_APP_PIPCHAT_SERVER_URL
   : 'http://localhost:3001');
